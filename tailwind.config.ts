ac57636211f80b9e8c9a5115fbe94d3c3afd8e14
IvanOrsh <ksironov@mail.ru>
2023-11-29 13:11:35 +0300

    chore: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,45 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
+  mode: "jit",
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+
   theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "960px",
+      xl: "1310px",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        artistOverlay: "url(/assets/hero/artist-overlay.png)",
+        newsletter: "url(/assets/newsletter/bg.png)",
+      },
+      fontFamily: {
+        alexBrush: ["--font-alexBrush"],
+        monserrat: ["--font-monserrat", "sans-serif"],
+      },
+      colors: {
+        primary: "#06062A",
+        secondary: "#151538",
+        tertiary: "#242445",
+        accent: {
+          DEFAULT: "#7f1cfc",
+          hover: "#6519c6",
+        },
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("tailwind-scrollbar")],
+};
+export default config;
