0ce53cb0981aa18835996903e21a005b948b4a8a
IvanOrsh <ksironov@mail.ru>
2023-11-30 00:27:53 +0300

    feat: hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 
 import { Events } from "@/components/events";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main>
+      <Hero />
       <Events />
     </main>
   );
