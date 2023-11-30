da8523b7ad63f96a70b914f0c1c7478ace91bee0
IvanOrsh <ksironov@mail.ru>
2023-11-30 14:57:22 +0300

    feat: player

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 
 import { Events } from "@/components/events";
 import Hero from "@/components/Hero";
+import Player from "@/components/Player";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Player />
       <Events />
     </main>
   );
---
+++ src/components/Player.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Image from "next/image";
+import { AudioPlayer } from "react-audio-play";
+import { motion } from "framer-motion";
+
+import { fadeIn } from "@/shared/variants";
+
+export default function Player() {
+  return (
+    <div className="section mx-auto mt-20 bg-gradient-to-r from-tertiary/70 to-primary/10 backdrop-blur-0 flex items-center ">
+      <div className="container mx-auto flex flex-col items-center xl:flex-row">
+        {/* text & avatar */}
+        <div className="hidden w-[300px] xl:flex items-center gap-x-4">
+          <div className="relative w-16 h-16">
+            <Image src="/assets/player/avatar.png" alt="avatar" fill />
+          </div>
+
+          <div className="leading-none space-y-1">
+            <div className="text-lg font-medium">Mia Reynolds</div>
+            <div className="text-sm font-light">Freedom</div>
+          </div>
+        </div>
+
+        {/* player */}
+        <div className="w-full max-w-4xl">
+          <AudioPlayer
+            loop
+            preload="none"
+            color="#333"
+            volume={40}
+            volumePlacement="top"
+            src="/assets/freedom.mp3"
+            style={{
+              marginInline: "auto",
+              color: "white",
+              background: "transparent",
+              boxShadow: "none",
+              width: "100%",
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
